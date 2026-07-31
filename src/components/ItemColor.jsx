9ab5f2a3ea027378131fa38fc3e951dbf39eac6f
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-31 19:25:22 -0300

    centramos la tabla de colores

--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -1,4 +1,4 @@
-const ItemColor = ({textoColorProps, deleteColorProps} ) => {
+const ItemColor = ({ textoColorProps, deleteColorProps }) => {
   const traductorColores = {
     rojo: "red",
     azul: "blue",
@@ -13,33 +13,41 @@ const ItemColor = ({textoColorProps, deleteColorProps} ) => {
     marron: "brown",
     morado: "purple",
     celeste: "skyblue",
-    turquesa: "turquoise"
-  }
+    turquesa: "turquoise",
+  };
   const colorLimpio = textoColorProps.toLowerCase().trim();
   const colorFinalCSS = traductorColores[colorLimpio] || colorLimpio;
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center gap-3 border-secondary mb-2">
-<div  className="d-flex align-items-center flex-grow-1 border border-secondary rounded p-1">
-        <div 
-          style={{ 
-            backgroundColor: colorFinalCSS, 
-            width: '20%', 
-            height: '40px', 
-            borderRadius: '4px', 
-            border: '1px solid #dee2e6', 
-            boxShadow: 'inset 0 1px 3px rgba(0,0,0,0.1)' 
-          }} 
+      <div
+        className="d-flex align-items-center justify-content-center  border border-secondary rounded p-1 flex-grow-1"
+        style={{ minHeight: "50px" }}
+      >
+        <div
+          style={{
+            backgroundColor: colorFinalCSS,
+            width: "20%",
+            height: "40px",
+            borderRadius: "4px",
+            border: "1px solid #dee2e6",
+            boxShadow: "inset 0 1px 3px rgba(0,0,0,0.3)",
+          }}
         />
-        <div className="flex-grow-1 text-center"> 
-      <span className="small text-capitalize text-light fw-semibold"
-       style={{ fontSize: "0.9rem" }}>
-          {textoColorProps}
-        </span>
-      </div>  
+        <div className="flex-grow-1  ms-3 px-2">
+          <span
+            className="small text-capitalize text-light fw-semibold"
+            style={{ fontSize: "0.9rem" }}
+          >
+            {textoColorProps}
+          </span>
+        </div>
       </div>
-    <button className="btn btn-danger px-3"
-       onClick={()=> deleteColorProps(textoColorProps)}> 
-       Borrar </button>
+      <button
+        className="btn btn-danger px-3"
+        onClick={() => deleteColorProps(textoColorProps)}
+      >
+        Borrar{" "}
+      </button>
     </li>
   );
 };
