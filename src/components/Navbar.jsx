7ac29f62f49c5523f1a8cf0b650cd659d0647b62
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-07-31 20:41:21 -0300

    commit para corregir

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          aria-controls="navbarSupportedContent" 
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
